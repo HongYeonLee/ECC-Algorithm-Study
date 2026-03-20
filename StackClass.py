e1752173d5b2d28ce8955f5b5e6b6135dc869219
HongYeonLee <llhongyan@naver.com>
2026-03-20 22:19:52 +0900

    클래스로 스택 구현하기

---
+++ StackClass.py
@@ -0,0 +1,55 @@
+class ArrayStack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.array = [None]*self.capacity
+        self.top = -1
+
+    def isEmpty(self):
+        return self.top == -1
+    
+    def isFull(self):
+        return self.top == self.capacity - 1
+    
+    def push(self, item):
+        if not self.isFull():
+            self.top += 1
+            self.array[self.top] = item
+        else:
+            pass
+    
+    def pop(self):
+        if not self.isEmpty():
+            self.top -= 1
+            return self.array[self.top + 1]
+        else:
+            pass
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.array[self.top]
+        else:
+            pass
+    
+    def size(self):
+        return self.top + 1
+    
+    def clear(self):
+        self.top = -1
+
+if __name__ == "__main__":
+    stack = ArrayStack(100)
+    
+    message = input("문자열 입력: ")
+    for char in message:
+        stack.push(char)
+
+    print(stack.top)
+    
+    print("문자열 거꾸로 출력: ", end='')
+    while not stack.isEmpty():
+        print(stack.pop(), end='')
+
+    
+    
+
+            
