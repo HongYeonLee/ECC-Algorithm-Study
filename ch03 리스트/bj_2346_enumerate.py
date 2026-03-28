c0f70d7c666d38bcac6a4e96c02f212e3924d339
HongYeonLee <llhongyan@naver.com>
2026-03-29 02:58:52 +0900

    docs: 주석 설명 구체화 (위치 -> 번호)

--- ch03 리스트/bj_2346_enumerate.py
+++ ch03 리스트/bj_2346_enumerate.py
@@ -2,7 +2,7 @@ import collections
 
 n = int(input())
 paper = list(map(int, input().split())) #풍선 안의 종이에 적힌 수를 받음
-#덱에 풍선의 위치(인덱스)와 풍선의 종이에 적힌 수를 튜플 형태로 저장함
+#덱에 풍선의 번호(인덱스)와 풍선의 종이에 적힌 수를 튜플 형태로 저장함
 # ex) ([(1, 3), (2, 2), (3, 1), (4, -3), (5, -1)])
 dq = collections.deque((i, p) for i, p in enumerate(paper, start=1)) #start=1은 인덱스를 1부터 시작하라는 의미
 result = []
