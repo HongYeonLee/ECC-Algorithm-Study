ba1869e406f899c879a6d4ab65d7dd2dcd97bf24
HongYeonLee <llhongyan@naver.com>
2026-05-02 01:38:28 +0900

    삽입 정렬 구현하기

--- ch06_정렬/simpleSort.py
+++ ch06_정렬/simpleSort.py
@@ -8,7 +8,24 @@ def selection_sort(A):
         A[i], A[least] = A[least], A[i] # A[i]와 A[least] 교환
         print("Step %2d = "%(i+1), A) #단계별 리스트 변화 출력
 
+def insertion_sort(A):
+    n = len(A)
+    for i in range(1, n): #i: 1 ~ n-1
+        key = A[i] #삽입할 요소를 미리 key에 저장
+        j = i - 1 #삽입할 요소부터 앞으로 가면서 값 비교 (뒤에서부터 비교하기)
+        while j >= 0 and A[j] > key: #i - 1요소부터 비교하여 앞으로 진행하는데, 이 요소가 key값 보다 크면 뒤로 한 칸 옮김
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = key
+        print("Step %2d = "%(i+1), A) #단계별 리스트 변화 출력
+
+
 data = [6, 3, 7, 4, 9, 1, 5, 2, 8]
 print("Original : ", data)
 selection_sort(data)
 print("Selection : ", data)
+
+data = [6, 3, 7, 4, 9, 1, 5, 2, 8]
+print("Original : ", data)
+insertion_sort(data)
+print("insertion : ", data)
