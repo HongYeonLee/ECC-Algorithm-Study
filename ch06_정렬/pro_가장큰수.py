1f2898a1dff2d6846a958217f4d1b63493d6a5d5
HongYeonLee <llhongyan@naver.com>
2026-05-12 05:25:49 +0200

    docs: 주석 수정

--- ch06_정렬/pro_가장큰수.py
+++ ch06_정렬/pro_가장큰수.py
@@ -3,7 +3,7 @@ from functools import cmp_to_key
 def compare(a, b):
     if a + b > b + a: #a = 3, b = 30일 때, 330 > 303 이므로 정렬시 a, b순서로 되어야함
         return -1 #-1의 의미: 입력값 a, b 중 a가 b보다 작으므로 오름차순 정렬시 a, b로 정렬
-    elif a + b < b + a: #a=5, b=9일 때, 59 < 95이므로 정렬시 b, a 순서로 되어야함 (b가 a보다 작다고 해야함)
+    elif a + b < b + a: #a=5, b=90일 때, 590 < 905이므로 정렬시 b, a 순서로 되어야함 (b가 a보다 작다고 해야함)
         return 1 #1의 의미: 입력값 a, b 중 a가 b보다 크므로 오름차순 정렬시 b, a로 정렬
     else:
         return 0 #0의 의미: 입력값 a=b이므로 순서를 그대로 둠
